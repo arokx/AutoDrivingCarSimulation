f623369f96bbc48cabd5a4c2b2d8111bb92934e2
ANUJA\anuja <anujachirantha@gmail.com>
2023-12-14 22:47:54 +0530

    add unit test

--- AutoDrivingCarSimulation.Tests/CarServiceTests.cs
+++ AutoDrivingCarSimulation.Tests/CarServiceTests.cs
@@ -1,16 +1,68 @@
+using Application.Services;
+using Domain.Entities;
+using static Domain.Entities.Enums.DomainEnums;
+
 namespace AutoDrivingCarSimulation.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class CarServiceTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void MoveForward_ShouldUpdatePosition()
         {
+            // Arrange
+            var carService = new CarService();
+            var currentPosition = new Position(1, 2, Direction.North);
+
+            // Act
+            var newPosition = carService.MoveForward(currentPosition, 10, 10);
+
+            // Assert
+            Assert.That(newPosition.X, Is.EqualTo(1));
+            Assert.That(newPosition.Y, Is.EqualTo(3));
+            Assert.That(newPosition.Facing, Is.EqualTo(Direction.North));
         }
 
         [Test]
-        public void Test1()
+        public void RotateLeft_ShouldRotateCorrectly()
         {
-            Assert.Pass();
+            // Arrange
+            var carService = new CarService();
+
+            // Act
+            var newDirection = carService.RotateLeft(Direction.North);
+
+            // Assert
+            Assert.That(newDirection, Is.EqualTo(Direction.West));
+        }
+
+        [Test]
+        public void RotateRight_ShouldRotateCorrectly()
+        {
+            // Arrange
+            var carService = new CarService();
+
+            // Act
+            var newDirection = carService.RotateRight(Direction.North);
+
+            // Assert
+            Assert.That(newDirection, Is.EqualTo(Direction.East));
+        }
+
+        [Test]
+        public void MoveForward_ShouldIgnoreOutOfBounds()
+        {
+            // Arrange
+            var carService = new CarService();
+            var currentPosition = new Position(0, 0, Direction.South);
+
+            // Act
+            var newPosition = carService.MoveForward(currentPosition, 10, 10);
+
+            // Assert
+            Assert.That(newPosition.X, Is.EqualTo(0));
+            Assert.That(newPosition.Y, Is.EqualTo(0));
+            Assert.That(newPosition.Facing, Is.EqualTo(Direction.South));
         }
     }
 }
