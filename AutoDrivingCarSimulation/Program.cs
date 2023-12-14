f611c3b6a724211c0ff279cf3033cb76f26c14df
ANUJA\anuja <anujachirantha@gmail.com>
2023-12-14 23:53:03 +0530

    handle invalid inputs

--- AutoDrivingCarSimulation/Program.cs
+++ AutoDrivingCarSimulation/Program.cs
@@ -14,28 +14,41 @@ public class Program
     }
     static void Main(string[] args)
     {
-        var carService = new CarService();
-        var simulationService = new SimulationService(carService);
-
-        // Read input
-        Console.WriteLine("Enter the width and height of the field");
-        string[] fieldSize = Console.ReadLine()?.Split() ?? Array.Empty<string>();
-        int width = int.Parse(fieldSize[0]);
-        int height = int.Parse(fieldSize[1]);
-
-        Console.WriteLine("Enter the current position and facing direction of the car");
-        string[] initialPosition = Console.ReadLine()?.Split() ?? Array.Empty<string>();
-        int x = int.Parse(initialPosition[0]);
-        int y = int.Parse(initialPosition[1]);
-        char direction = char.Parse(initialPosition[2]);
-
-        Console.WriteLine("Enter the commands");
-        string commands = Console.ReadLine() ?? string.Empty;
+        try
+        {
 
 
-        var finalPosition = simulationService.RunSimulation(width, height, x, y, GetDirection(direction), commands);
+            var carService = new CarService();
+            var simulationService = new SimulationService(carService);
 
-        Console.WriteLine($"{finalPosition.X} {finalPosition.Y} {finalPosition.Facing}");
+            // Read input
+            Console.WriteLine("Enter the width and height of the field");
+            string[] fieldSize = Console.ReadLine()?.Split() ?? Array.Empty<string>();
+            int width = int.Parse(fieldSize[0]);
+            int height = int.Parse(fieldSize[1]);
+
+            Console.WriteLine("Enter the current position and facing direction of the car");
+            string[] initialPosition = Console.ReadLine()?.Split() ?? Array.Empty<string>();
+            int x = int.Parse(initialPosition[0]);
+            int y = int.Parse(initialPosition[1]);
+            char direction = char.Parse(initialPosition[2]);
+
+            Console.WriteLine("Enter the commands");
+            string commands = Console.ReadLine() ?? string.Empty;
+
+
+            var finalPosition = simulationService.RunSimulation(width, height, x, y, GetDirection(direction), commands);
+
+            Console.WriteLine($"{finalPosition.X} {finalPosition.Y} {finalPosition.Facing}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Invalid Inputs! Please ensure your input follows the correct format:");
+            Console.WriteLine("Field: width height (e.g., 10 10)");
+            Console.WriteLine("Position: x y direction (e.g., 1 2 N)");
+            Console.WriteLine("Commands: a sequence of L, R, and F (e.g., FFRFFFRRLF)");
+        }
     }
 
     public static DomainEnums.Direction GetDirection(char direction)
