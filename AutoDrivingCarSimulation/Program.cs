08b99c76d06350146347abfc2b45a2bc6e6ede5e
ANUJA\anuja <anujachirantha@gmail.com>
2023-12-14 23:40:21 +0530

    read inputs in console

--- AutoDrivingCarSimulation/Program.cs
+++ AutoDrivingCarSimulation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Interfaces;
 using Application.Services;
 using Domain.Entities;
+using Domain.Entities.Enums;
 using static Domain.Entities.Enums.DomainEnums;
 
 public class Program
@@ -16,9 +17,41 @@ public class Program
         var carService = new CarService();
         var simulationService = new SimulationService(carService);
 
+        // Read input
+        Console.WriteLine("Enter the width and height of the field");
+        string[] fieldSize = Console.ReadLine()?.Split() ?? Array.Empty<string>();
+        int width = int.Parse(fieldSize[0]);
+        int height = int.Parse(fieldSize[1]);
 
-        var finalPosition = simulationService.RunSimulation(10, 10, 1, 2, Direction.North, "FFRFFFRRLFLF");
+        Console.WriteLine("Enter the current position and facing direction of the car");
+        string[] initialPosition = Console.ReadLine()?.Split() ?? Array.Empty<string>();
+        int x = int.Parse(initialPosition[0]);
+        int y = int.Parse(initialPosition[1]);
+        char direction = char.Parse(initialPosition[2]);
+
+        Console.WriteLine("Enter the commands");
+        string commands = Console.ReadLine() ?? string.Empty;
+
+
+        var finalPosition = simulationService.RunSimulation(width, height, x, y, GetDirection(direction), commands);
 
         Console.WriteLine($"{finalPosition.X} {finalPosition.Y} {finalPosition.Facing}");
+    }
+
+    public static DomainEnums.Direction GetDirection(char direction)
+    {
+        switch (direction)
+        {
+            case 'W':
+                return Direction.West;
+            case 'S':
+                return Direction.South;
+            case 'E':
+                return Direction.East;
+            case 'N':
+                return Direction.North;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction));
+        }
     }
 }
